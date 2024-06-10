4753d7168c7a9f1de5d6cb49676dca460870d0eb
gulzigit22 <talantbekuulugulzigit51@gmail.com>
2024-06-10 12:28:48 +0600

    red color

--- lib/view/ekran_2.dart
+++ lib/view/ekran_2.dart
@@ -6,7 +6,7 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.red,
       appBar: AppBar(
         actions: [
           SizedBox(
